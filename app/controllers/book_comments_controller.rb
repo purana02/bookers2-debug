a7705cc471e094672feb08dbff19d86c6dcea5f3
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-03-24 12:16:11 +0000

    [Add]ouyou6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,15 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back fallback_location: root_path
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back fallback_location: root_path
+    @book=Book.find(params[:book_id])
+    @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book_comment = BookComment.new
   end
 
   private
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @booknew = Book.new
-    @comment = BookComment.new
+    @book_comment = BookComment.new
   end
 
   def index
