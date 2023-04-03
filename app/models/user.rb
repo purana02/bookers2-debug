518a5cb411342bcc1efab175c393c201352e8742
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-04-03 13:11:50 +0000

    [Add]8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,9 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
-
+  has_many :user_rooms
+  has_many :chats
+  has_many :rooms, through: :user_rooms
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
