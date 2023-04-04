20458016d6a6914212fab0d3c7c52e119d21f601
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-04-04 04:11:58 +0000

    [Add]8a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+ before_action :authenticate_user!
  before_action :ensure_currect_user, only: [:update,:edit]
   def show
     @book = Book.find(params[:id])
@@ -8,8 +9,13 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
-    @book=Book.new
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
+    @book = Book.new
   end
 
   def create
